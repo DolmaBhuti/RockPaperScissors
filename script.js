7062d1017e940d67497782050b8663ab69c23d08
Dolma Bhuti <dbhuti95@gmail.com>
2023-03-31 14:47:32 -0400

    game function with loop added

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var getComputerChoice = function () {
   let randomNumber = Math.floor(Math.random() * 3) + 1; //The following would give us a random number between 1 and 3:
 
   if (randomNumber === 1) {
-    console.log("Rock");
+    console.log("Computer Chooses Rock");
     return "Rock";
   } else if (randomNumber === 2) {
-    console.log("Paper");
+    console.log("Computer Chooses Paper");
     return "Paper";
   } else if (randomNumber === 3) {
-    console.log("Scissors");
+    console.log("Computer Chooses Scissors");
     return "Scissors";
   }
 };
@@ -44,6 +44,37 @@ var playRockPaperScissors = (playerSelection, computerSelection) => {
   }
 };
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRockPaperScissors(playerSelection, computerSelection));
+var game = function () {
+  let count = 0;
+  let playerScore = 0;
+  let compScore = 0;
+
+  while (count < 5) {
+    //Use prompt() to get input from the user.
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    const computerSelection = getComputerChoice();
+    let result = playRockPaperScissors(playerSelection, computerSelection);
+
+    //display the results of each round
+    console.log(result);
+    if (result.toLowerCase().includes("win")) {
+      ++playerScore;
+      ++count;
+    } else if (result.toLowerCase().includes("lose")) {
+      {
+        ++compScore;
+        ++count;
+      }
+    } else {
+      console.log("It's a tie");
+    }
+  }
+  //display the winner at the end
+  if (playerScore > compScore) {
+    console.log(`You Win! You have ${playerScore} points`);
+  } else {
+    console.log(`You Lose! You have ${playerScore} points`);
+  }
+  return;
+};
+game();
